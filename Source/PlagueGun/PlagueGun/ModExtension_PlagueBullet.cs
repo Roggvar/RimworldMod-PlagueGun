c2c950638c1dccacda95a8439120cd04c05b4820
Vitor Galante <vccastro.galante@gmail.com>
2021-09-13 10:56:08 -0300

    Completely Redesign to Version 1.3

---
+++ Source/PlagueGun/PlagueGun/ModExtension_PlagueBullet.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace PlagueGun
+{
+    public class ModExtension_PlagueBullet : DefModExtension
+    {
+
+        public float addHediffChance = 1f;
+        public HediffDef hediffToAdd;
+
+    }
+}
